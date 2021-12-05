332473a976b83a89d2a99a54d94f3e155a3dec0d
rgksekhar <rgksekhar@gmail.com>
2021-12-05 06:43:01 +0530

    Added unit tests .

---
+++ MonthlyPremiumCalculator.UnitTests/Services/Repository/OccupationRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MonthlyPremiumCalculatorDtos;
+using MonthlyPremiumCalculatorServices.Repository;
+using NUnit.Framework;
+
+namespace MonthlyPremiumCalculator.UnitTests
+{
+    [TestFixture]
+    public class OccupationRepositoryTests
+    {      
+        private OccupationRepository _occupationRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _occupationRepository = new OccupationRepository();
+        }
+
+        [Test]
+        public async Task GetOccupationList()
+        {
+            var request = new PremiumCalculatorDtos() { SumInsured = 100000, Age = 33 };
+            var result = await _occupationRepository.GetOccupations();
+
+            Assert.AreEqual(7, result.Count());
+        }
+    }
+}
