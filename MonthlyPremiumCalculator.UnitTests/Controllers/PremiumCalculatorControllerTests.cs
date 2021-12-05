63e429e5ca1427291b1d313f4830346301f7808e
rgksekhar <rgksekhar@gmail.com>
2021-12-05 06:09:58 +0530

    Added unit test project.

---
+++ MonthlyPremiumCalculator.UnitTests/Controllers/PremiumCalculatorControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MonthlyPremiumCalculator.Controllers;
+using MonthlyPremiumCalculatorDtos;
+using MonthlyPremiumCalculatorServices.Services;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MonthlyPremiumCalculator.UnitTests
+{
+    [TestFixture]
+    public class PremiumCalculatorControllerTests
+    {
+        private IPremiumCalculatorService _premiumCalculatorService;
+        private PremiumCalculatorController _premiumCalculatorController;
+        private ILogger<PremiumCalculatorController> _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _premiumCalculatorService = Substitute.For<IPremiumCalculatorService>();
+            _logger = Substitute.For<ILogger<PremiumCalculatorController>>();
+
+            _premiumCalculatorController = new PremiumCalculatorController(_premiumCalculatorService, _logger);
+        }
+
+        [Test]
+        public async Task GetOccupationList()
+        {
+            var request = new PremiumCalculatorDtos();
+            _premiumCalculatorService.CalculatePremium(request).Returns(100);
+
+            var result = (OkObjectResult)await _premiumCalculatorController.CalculatePremium(request);
+
+            Assert.AreEqual(200, result.StatusCode);
+            Assert.AreEqual(100, result.Value);
+        }
+    }
+}
