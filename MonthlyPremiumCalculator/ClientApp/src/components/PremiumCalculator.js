424ee4d53c2f93b481f7ba4d27c96065867f44b1
rgksekhar <rgksekhar@gmail.com>
2021-12-06 12:15:17 +0530

    Added Reset button,  fixed unit test cases and formatted.

--- MonthlyPremiumCalculator/ClientApp/src/components/PremiumCalculator.js
+++ MonthlyPremiumCalculator/ClientApp/src/components/PremiumCalculator.js
@@ -91,143 +91,167 @@ export class PremiumCalculator extends Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
+    resetFields() {
+        this.setState(this.initialState);
+        this.populateOccupationsData();
+    }
+
     render() {
         const { nameErr, ageErr, dobErr, sumInsuredErr, ageErrorMessage } = this.state.formErrors;
         return (
-            <div className="container">
-                <div className="heading"> Monthly Premium Calculator</div>
-                <div className="row">
-                    <div className="col-sm-4">
-                        <label className={"labelDiv"}>
-                            Name <span className="mandatory" >*
+            <div className = "container">
+                <div className = "heading"> Monthly Premium Calculator</div>
+                <div className = "row">
+                    <div className = "col-sm-4">
+                        <label className = {"labelDiv"}>
+                            Name <span className = "mandatory" >*
                             </span >
                         </label></div>
-                    <div className="col-sm-4" >
-                        <input className="inputDiv" type="text" name="name" minLength="O" maxLength="40" placeholder="Your Name.."
-                            onChange={(e) => this.handleChange(e)}
-                            value={this.state.name} className={nameErr ? 'showError' : ''} />
+                    <div className = "col-sm-4" >
+                        <input className = "inputDiv"
+                            type = "text"
+                            name = "name"
+                            minLength = "O"
+                            maxLength = "40"
+                            placeholder= "Your Name.."
+                            onChange = {(e) => this.handleChange(e)}
+                            value = { this.state.name }
+                            className = { nameErr ? 'showError' : ''} />
                         
                         {
                             nameErr &&
-                            <div className="mandatory">{nameErr}</div>
+                            <div className = "mandatory"> { nameErr } </div>
                         }
 
                     </div >
                 </div >
-                <div className="row pt-2" >
-                    <div className="col-sm-4" >
+                <div className = "row pt-2" >
+                    <div className = "col-sm-4" >
                         <label>
-                            Age<span className="mandatory">*
+                            Age<span className = "mandatory">*
                             </ span>
 
                         </label></div >
 
-                    <div className="col-sm-4" >
+                    <div className = "col-sm-4" >
                         <input
-                            className="inputDiv"
-                            type="text"
-                            name="age"
-                            type="number" maxLength="3"
-                            placeholder="Your Age.."
-                            onKeyPress={(e) => this.ageHandler(e.target.value)}
-                            onChange={(e) => this.handleChange(e)}  value={this.state.age} />
+                            className = "inputDiv"
+                            type = "text"
+                            name = "age"
+                            type = "number" maxLength = "3"
+                            placeholder = "Your Age.."
+                            onKeyPress = {(e) => this.ageHandler(e.target.value)}
+                            onChange = {(e) => this.handleChange(e)}
+                            value = {this.state.age} />
 
                         {
                             this.state.age.length < 1 && this.state.errMsg ?
 
-                                (<div className="mandatory">Please enter age</div>) : this.state.ageError &&
+                                (<div className = "mandatory">Please enter age</div>) : this.state.ageError &&
 
                                     (parseInt(this.state.age) > 100
                                         ||
                                         parseInt(this.state.age) < 0) ?
 
-                                    <div className="mandatory" > Please enter your age between 0 and 100.
+                                    <div className = "mandatory" > Please enter your age between 0 and 100.
                                     </div > : null
                         }
                         {
                             ageErr &&
-                            <div className="mandatory">{ageErr}</div>
+                            <div className = "mandatory">{ageErr}</div>
                         }
                     </div >
 
-
                 </div>
-                <div className="row pt-2" >
-                    <div className="col-sm-4" >
-                        <label className={"labelDiv"}>
+                <div className = "row pt-2" >
+                    <div className = "col-sm-4" >
+                        <label className = {"labelDiv"}>
 
-                            Date <span className="mandatory" >*
+                            Date <span className = "mandatory" >*
                             </span>
                         </label ></div >
 
-                    <div
-                        className="col-sm-4">
+                    <div className = "col-sm-4">
 
-                        <input
-                           
-                            className="inputDiv"
-                            name="dob"
-                            type="date" 
-                            placeholder="Your Dob.."
-                            onChange={(e) => this.handleChange(e)}
+                        <input                           
+                            className = "inputDiv"
+                            name = "dob"
+                            type = "date" 
+                            placeholder = "Your Dob.."
+                            onChange = {(e) => this.handleChange(e)}
                             required 
-                            value={this.state.dob}
-                        />
+                            value = {this.state.dob}  />
                         
                         {
                             dobErr &&
-                            <div className="mandatory">{dobErr}</div>
+                            <div className = "mandatory"> {dobErr} </div>
                         }
                     </div>
                 </div>
-                <div className="row pt-2">
-                    <div className="col-sm-4" >
-                        <label className={"labelDiv"}>
-                            Sum Insured <span className="mandatory">*
+                <div className = "row pt-2">
+                    <div className = "col-sm-4" >
+                        <label className = {"labelDiv"} >
+                            Sum Insured <span className = "mandatory">*
                             </span>
 
                         </label></div >
-                    <div className="col-sm-4" >
-                        <input className="inputDiv" name="sumInsured"
-                            type="number"
-                            placeholder="Sum Insured.."
-                            onChange={(e) => this.handleChange(e)} value = {this.state.sumInsured}
-                            minLength="O" maxLength="6"  />
+                    <div className = "col-sm-4" >
+                        <input className="inputDiv"
+                            name="sumInsured"
+                            type = "number"
+                            placeholder = "Sum Insured.."
+                            onChange = {(e) => this.handleChange(e)}
+                            value = {this.state.sumInsured}
+                            minLength = "O" maxLength = "6"  />
                         {
                             sumInsuredErr || parseInt((this.state.sumInsured) < 0) ?
 
-                                (<div className="mandatory">{sumInsuredErr}</div>) : null
+                                (<div className = "mandatory">{sumInsuredErr}</div>) : null
                         }
                     </div >
 
                 </div >
-                <div className="row pt-2" >
-                    <div className="col-sm-4"> <label>
-                        Occupation <span className="mandatory">* </span>
+                <div className = "row pt-2" >
+                    <div className = "col-sm-4"> <label>
+                        Occupation <span className = "mandatory">* </span>
                     </label ></div >
-                    <div className="col-sm-4" >
+                    <div className = "col-sm-4" >
                         <div>
                             <select
-                                className="inputDiv"
-                                name="list"
-                                id="list" onChange={this.CalculatePremium}>
+                                className = "inputDiv"
+                                name = "list"
+                                id="list"
+                                onChange={this.CalculatePremium}>
                                 {
                                         this.state.occupations.map((value, index) => (
-                                        <option key={index} value={value} >
+                                        <option key = {index} value = {value} >
                                             {value}
                                         </option>))}
                             </select>
                         </div >
                     </div >
                 </div>
-                <div className="row pt-2" >
-                    <div className="col-sm-4" >
+                <div className = "row pt-2" >
+                    <div className = "col-sm-4" >
                         Total Premium amount
                     </div>
 
-                    <div className="col-sm-4">
-                        <span className="inputDiv" >
+                    <div className = "col-sm-4">
+                        <span className = "inputDiv" >
                             ${this.state.premiumAmount} </span>
+                    </div>
+                </div >
+
+                <div className = "row pt-2" >
+                    <div className = "col-sm-4" >
+                       </div>
+
+                    <div className = "col-sm-6">
+                        <input
+                            className = "btn-primary"
+                            type = "button"
+                            value = "Reset"
+                            onClick={() => { this.resetFields() }} />
                     </div>
                 </div >
             </div>
